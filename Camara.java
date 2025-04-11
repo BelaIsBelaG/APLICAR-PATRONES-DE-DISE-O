9bb273b1228bf9a8cbf85e02a572b4c7924c49d8
Isabela Gonzalez <159276237+BelaIsBelaG@users.noreply.github.com>
2025-04-11 08:41:47 +0000

    Documentacion de la clase Camara

--- Camara.java
+++ Camara.java
@@ -1,14 +1,27 @@
-// Producto tipo Camara
+/**
+ * Clase concreta que representa un producto del tipo "Cámara".
+ */
 class Camara extends Producto {
     String marca;
     String modelo;
 
+    /**
+     * Constructor para crear una nueva cámara.
+     *
+     * @param numero Número del producto en el pedido.
+     * @param marca  Marca de la cámara.
+     * @param modelo Modelo de la cámara.
+     */
     public Camara(int numero, String marca, String modelo) {
         this.numero = numero;
         this.marca = marca;
         this.modelo = modelo;
     }
 
+    /**
+     * Muestra los detalles específicos de una cámara.
+     * Implementación concreta del método abstracto.
+     */
     @Override
     public void mostrarDetalles() {
         System.out.println("Cámara #" + numero + ", Marca: " + marca + ", Modelo: " + modelo);
