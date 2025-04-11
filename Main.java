bc86ea674d54b56ca457ac3ffdb57c0bdc04e47e
AnyeliA <164533759+AnyeliA@users.noreply.github.com>
2025-04-11 06:19:49 +0000

    creacion de la clase Main

---
+++ Main.java
@@ -0,0 +1,21 @@
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        // Crear cliente
+        Cliente cliente = new Cliente("0012345678", "Ana Torres");
+
+        // Crear pedido con productos usando el Builder
+        Pedido pedido = new PedidoBuilder()
+                .setCliente(cliente)
+                .setFecha(new Date())
+                .setTelefono(123456789)
+                .agregarCamara(1, "Canon", "EOS 90D")
+                .agregarImpresion(2, "Color", new Foto("foto_playa.jpg"))
+                .agregarImpresion(3, "Blanco y Negro", new Foto("foto_montaña.png"))
+                .build();
+
+        // Mostrar detalles del pedido
+        pedido.mostrarPedido();
+    }
+}
