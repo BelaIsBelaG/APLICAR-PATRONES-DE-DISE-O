3bacd4d026d6349a244fd37ed05b6df5c3823224
Isabela Gonzalez <159276237+BelaIsBelaG@users.noreply.github.com>
2025-04-11 08:30:23 +0000

    Documentacion de la clase Cliente

--- Cliente.java
+++ Cliente.java
@@ -1,14 +1,27 @@
+/**
+ * Clase que representa a un cliente del sistema.
+ */
 public class Cliente{
 
     private String cedula;
     private String nombre;
 
-public Cliente(String cedula, String nombre) {
+    /**
+     * Constructor para inicializar los datos del cliente.
+     *
+     * @param cedula Cédula del cliente.
+     * @param nombre Nombre del cliente.
+     */
+
+    public Cliente(String cedula, String nombre) {
         this.cedula = cedula;
         this.nombre = nombre;
     }
+   /**
+     * Muestra la información del cliente por consola.
+     */
 
- public void mostrarCliente() {
+    public void mostrarCliente() {
         System.out.println("Cedula: " + cedula);
         System.out.println("Nombre: " + nombre);
     }
