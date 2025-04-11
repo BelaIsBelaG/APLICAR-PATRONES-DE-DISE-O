87b63783292872210fdc7ae912cdcb819ed87b0a
Isabela Gonzalez <159276237+BelaIsBelaG@users.noreply.github.com>
2025-04-11 08:40:12 +0000

    Documentacion de la clase hija Impresion

--- Impresion.java
+++ Impresion.java
@@ -1,14 +1,28 @@
-// Producto tipo Impresion (usa composición con Foto)
+/**
+ * Clase concreta que representa un producto del tipo "Impresión".
+ * Aplica el principio de composición al incluir un objeto de tipo Foto.
+ */
 class Impresion extends Producto {
     String color;
     Foto foto;
 
+    /**
+     * Constructor para crear una nueva impresión.
+     *
+     * @param numero Número del producto en el pedido.
+     * @param color  Tipo de color de la impresión.
+     * @param foto   Objeto Foto que contiene el archivo a imprimir.
+     */
     public Impresion(int numero, String color, Foto foto) {
         this.numero = numero;
         this.color = color;
         this.foto = foto;
     }
 
+     /**
+     * Muestra los detalles específicos de una impresión.
+     * Implementación concreta del método abstracto.
+     */
     @Override
     public void mostrarDetalles() {
         System.out.println("Impresión #" + numero + ", Color: " + color + ", Foto: " + foto.fichero);
