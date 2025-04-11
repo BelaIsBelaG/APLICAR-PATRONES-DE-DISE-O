0afe9ae397a6449fe5d614569bb7d080ce91d0c9
AnyeliA <164533759+AnyeliA@users.noreply.github.com>
2025-04-11 06:26:19 +0000

    creacion de la clase producto

---
+++ Producto.java
@@ -0,0 +1,7 @@
+//clase abstracta para todos los productos
+public abstract class Producto{
+    int numero;
+
+//metodo comun que se implementa de manera diferente en cada clase hija
+public abstract void mostrarDetalles();
+}
