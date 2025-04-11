8799420163c5be5ea7a8fa1caf1da5d4c844b57b
Isabela Gonzalez <159276237+BelaIsBelaG@users.noreply.github.com>
2025-04-11 08:32:16 +0000

    Documentacion de la clase Foto

--- Foto.java
+++ Foto.java
@@ -1,9 +1,21 @@
-class Foto {
-    String fichero;
+/**
+ * Clase simple que representa una foto digital.
+ */
 
+class Foto {
+    String fichero; //Nombre del archivo que representa la imagen
+    /**
+     * Constructor que recibe el nombre del fichero de imagen.
+     *
+     * @param fichero Nombre del archivo de foto.
+     */
     public Foto(String fichero) {
         this.fichero = fichero;
     }
+    
+    /**
+     * Simula la impresión de la foto por consola.
+     */
 
     public void imprimir() {
         System.out.println("Imprimiendo foto: " + fichero);
